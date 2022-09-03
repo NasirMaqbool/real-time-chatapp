97d5f0164ca2a20fbdbf516980382c23efe8528c
nasirmaqbool <nasirmaqbool271@gmail.com>
2022-09-02 16:26:09 -0800

    initial commit

---
+++ BE/index.js
@@ -0,0 +1,27 @@
+const express = require("express") 
+const { Server } = require("socket.io");
+const http = require("http")
+const cors = require ("cors")
+
+const app = express()
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http:/localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+io.on("connection", (socket) => {
+    console.log(socket.id); 
+
+    socket.on("joinRoom")
+  });
+
+app.get("/", (req, res)=>{
+    res.send("Socket chat be started.")
+})
+
+server.listen(8000, () => console.log("app started at port 8000"))
